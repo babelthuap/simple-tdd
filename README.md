619b4870a62727ebfdcabc1ffb72de1b74762005
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-12-14 14:04:22 -0800

    Add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+### Status
+[![Build Status](https://travis-ci.org/babelthuap/simple-tdd.png)](https://travis-ci.org/babelthuap/simple-tdd)
--- test/acceptance/books.js
+++ test/acceptance/books.js
@@ -20,23 +20,25 @@ describe('/books', function() {
     });
 
     it('should add a book to the database', function(done) {
+      var title = 'Test Title 1'
+        , author = 'Test Author 1';
       chai.request(app)
         .post('/books')
-        .send({title: 'Test Title 1', author: 'Test Author 1'})
+        .send({title: title, author: author})
         .end(function(err, res) {
           expect(err).to.be.null;
           expect(res).to.have.status(200);
           Book.findOne({}, function(err, book) {
             expect(res.body._id).to.be.ok;
-            expect(res.body.title).to.equal('Test Title 1');
-            expect(res.body.author).to.equal('Test Author 1');
+            expect(res.body.title).to.equal(title);
+            expect(res.body.author).to.equal(author);
             expect(book._id).to.be.ok;
-            expect(book.title).to.equal('Test Title 1');
-            expect(book.author).to.equal('Test Author 1');
+            expect(book.title).to.equal(title);
+            expect(book.author).to.equal(author);
             done();
           })
         })
-      
+
     })
   })
 });
